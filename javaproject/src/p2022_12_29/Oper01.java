80a8cf3fb3ca1c36514fde8358b74d541dbdd911
admin <admin@DESKTOP-PK8PLU4>
2022-12-30 10:21:01 +0900

    연산자
    산술연산자

---
+++ javaproject/src/p2022_12_29/Oper01.java
@@ -0,0 +1,22 @@
+package p2022_12_29;
+
+public class Oper01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//산술 연산자: +, -, *, /(몫), %(나머지)
+//int형과 int형을 산술연산을 수행하면, 결과는 int형으로 처리된다. 
+	int a =10, b = 3, c;
+	
+	c= a +b;
+	System.out.println("a+b="+c);
+	System.out.println("a+b="+(a+b));
+	System.out.println("a-b="+(a-b));
+	System.out.println("a*b="+(a*b));
+	System.out.println("a/b="+(a/b));
+	System.out.println("a%b="+(a%b));
+	
+	}
+
+}
