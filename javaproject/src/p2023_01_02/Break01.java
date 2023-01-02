e88fd419c3af42c6ca6530a33c712d4edbbbd066
admin <admin@DESKTOP-PK8PLU4>
2023-01-03 08:57:23 +0900

    break
    break를 이용해 for문 무한루프를 빠져나온다.

---
+++ javaproject/src/p2023_01_02/Break01.java
@@ -0,0 +1,26 @@
+package p2023_01_02;
+
+public class Break01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//무한로프: for문
+//break: 반복문을 빠져 나오는 역할		
+
+		for (int i = 1;; i++) {
+			if(i==100) {
+				System.out.println(i + "무한출력");
+				break;
+			
+			}
+
+				
+				
+
+			
+
+		}
+	}
+
+}
