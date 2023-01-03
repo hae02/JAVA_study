f11b616e81820d71b2105ba806116794e4d54b8d
admin <admin@DESKTOP-PK8PLU4>
2023-01-03 09:05:04 +0900

    for문
    for문을 활용하여 메세지 반복출력

---
+++ javaproject/src/p2023_01_02/For01.java
@@ -0,0 +1,22 @@
+package p2023_01_02;
+
+public class For01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		for문
+//		for(초기값; 조건식; 증감식){
+//			반복 실행할 문장;
+//		}
+
+//		사랑해요 메세지를 10를 10번 출력하세요
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " 사랑해요.");
+		}
+		
+
+	}
+
+}
