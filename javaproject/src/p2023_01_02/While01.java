b2b433a4b649ca87c2ceb34a9c2377dd565bd2ff
admin <admin@DESKTOP-PK8PLU4>
2023-01-03 09:15:44 +0900

    switch
    switch를 이용하여 키보드로 입력한 점수가 어느 학점에 해당하는지 출력하는 프로그램

---
+++ javaproject/src/p2023_01_02/While01.java
@@ -0,0 +1,25 @@
+package p2023_01_02;
+
+public class While01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		2. while문
+//	    while(조건식){
+//	       반복 실행할 문장;
+//	    }
+//while 문을 이용해서 '사랑해요' 메세지를 10번 출력하는 프로그램을 작성하세요
+		
+		int i = 1;  //초기값
+		while (i <=10) {  //조건식
+			System.out.println(i+". 사랑해요"); 
+			++i;  //증감식
+			
+			
+		}
+
+	}
+
+}
+ 
