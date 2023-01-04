4feaab5705bc5c99a6f0e22865f43338ac501c23
admin <admin@DESKTOP-PK8PLU4>
2023-01-04 17:43:52 +0900

    배열03
    키보드로 입력받아서 총점과 평균을 구하는 프로그램

---
+++ javaproject/src/p2023_01_04/ArrayEx03.java
@@ -0,0 +1,28 @@
+package p2023_01_04;
+
+import java.util.Scanner;
+
+public class ArrayEx03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//키보드로 5과목의 점수를 입력받아서, 총점과 평균을 구하는 프로그램을 작성
+		int[] s = new int[5];
+		System.out.println("5과목의 점수를 입력하세요");
+		Scanner sc = new Scanner(System.in);
+
+		int sum = 0;	//초기화
+		for (int i = 0; i < s.length; i++) {
+			s[i] = sc.nextInt();
+			sum +=s[i];		//sum = sum + s[i];
+		}
+		double avg = sum /5.0;
+		System.out.println("총점:" + sum);
+		System.out.println("평균:" + avg);
+		
+		sc.close();
+
+	}
+
+}
