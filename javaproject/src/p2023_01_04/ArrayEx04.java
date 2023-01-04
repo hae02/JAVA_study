78ef337933dbdf4aaf19ebc36dba910b219bf3e3
admin <admin@DESKTOP-PK8PLU4>
2023-01-04 17:43:03 +0900

    배열04
    배열에 저장된 데이터 중에서 최대값과 최소값을 구하는 프로그램

---
+++ javaproject/src/p2023_01_04/ArrayEx04.java
@@ -0,0 +1,30 @@
+package p2023_01_04;
+
+public class ArrayEx04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//배열에 저장된 데이터 중에서 최대값과 최소값을 구하는 프로그램을 작성하세요
+
+		double[] data = { 9.5, 7.0, 13.6, 7.5, 10.5 };
+
+		double max, min;
+
+		max = data[0];
+		min = data[0];
+
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] > max)
+				max = data[i];
+			if (data[i] < min)
+				min = data[i];
+
+		}
+
+		System.out.println("max:" + max);
+		System.out.println("min:" + min);
+
+	}
+
+}
